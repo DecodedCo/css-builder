4340dc6f533d9cdf7f6e279d023328384b52557d
Jonathan Garner <jonathan@jonathangarner.co.uk>
2016-06-09 17:06:52 +0100

    cleaned up & commented CSS fully

--- public/js/sticky-header.js
+++ public/js/sticky-header.js
@@ -1,7 +1,7 @@
-console.log("script loaded");
 $(function() {
-  console.log("waypoint created");
+  // When the page scrolls past the top of the main content
   $('.js-main-content').waypoint(function() {
+    // Apply the sticky class to the header so it follows you down the page
     $('.c-site-header').toggleClass('sticky');
   })
 });
